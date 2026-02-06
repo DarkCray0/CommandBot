39dd4d8d8fe44f823d41b16bf3e40238ad9b93d3
DarkCray0 <156240056+DarkCray0@users.noreply.github.com>
2026-02-06 17:35:24 -0330

    Update Aliases

--- src/client/java/me/darkcray_/commandbot/bot/BotCommandAliases.java
+++ src/client/java/me/darkcray_/commandbot/bot/BotCommandAliases.java
@@ -5,13 +5,11 @@ import java.util.Map;
 
 public class BotCommandAliases {
     public static final Map<String, List<String>> ALIASES = Map.ofEntries(
-            // Movement
             Map.entry("вперед", List.of("вперед", "вп", "forward", "fwd")),
             Map.entry("назад", List.of("назад", "нз", "back", "bk")),
             Map.entry("влево", List.of("влево", "вл", "left", "lt")),
             Map.entry("вправо", List.of("вправо", "вп", "right", "rt")),
 
-            // Hotbar slots
             Map.entry("слот-1", List.of("слот-1", "1", "slot-1")),
             Map.entry("слот-2", List.of("слот-2", "2", "slot-2")),
             Map.entry("слот-3", List.of("слот-3", "3", "slot-3")),
@@ -22,25 +20,20 @@ public class BotCommandAliases {
             Map.entry("слот-8", List.of("слот-8", "8", "slot-8")),
             Map.entry("слот-9", List.of("слот-9", "9", "slot-9")),
 
-            // Camera
             Map.entry("налево", List.of("налево", "нл", "turn-left", "cam-left")),
             Map.entry("направо", List.of("направо", "нп", "turn-right", "cam-right")),
             Map.entry("вверх", List.of("вверх", "вв", "up", "cam-up")),
             Map.entry("вниз", List.of("вниз", "вн", "down", "cam-down")),
 
-            // Click / Attack
             Map.entry("лкм", List.of("лкм", "lc", "left-click")),
             Map.entry("пкм", List.of("пкм", "rc", "right-click")),
             Map.entry("удар", List.of("удар", "atk", "attack")),
 
-            // Movement actions
             Map.entry("присесть", List.of("присесть", "shift", "sneak")),
             Map.entry("прыгнуть", List.of("прыгнуть", "jmp", "jump")),
 
-            // Other
             Map.entry("выбросить", List.of("выбросить", "drop")),
 
-            // Chat commands
             Map.entry("сесть", List.of("сесть", "sit")),
             Map.entry("лечь", List.of("лечь", "lay")),
             Map.entry("ползти", List.of("ползти", "crawl"))
